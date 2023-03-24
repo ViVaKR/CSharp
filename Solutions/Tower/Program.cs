30a6b3fbca5d0ddbdf1961c1cfd405952cfddeec
ViVaKR <admin@vivabj.com>
2023-03-25 00:36:38 +0900

    Update Tower

--- Solutions/Tower/Program.cs
+++ Solutions/Tower/Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿//========== 피라미드 공사 시작 ==========//
+// 구현 알고리즘 설명: 
+// -> 다양한 층수 구현을 위하여 입력된 층수를 지정
+// -> 구현 클래스 라이브러리의, Recurstion (재귀함수)를 이용하여 
+// -> 층별 string 을 List<string> 에 저장한 후
+// -> 리스트로 부터 취향에 맞게 출력하는 알고리즘
+// 피라미드 구현 클래스 인스턴스 생성 //
 
-// 피라미드 구현 클래스 인스턴스 생성
-var tower = new Tower(7);
+const int plan = 9;
+var tower = new Tower(plan);
 
-// 한줄 띄기
+// 한줄 모냥내기
 Console.WriteLine();
 
-// 촘촘하고 사이사이 라인이 그러진 피라미드 만들기
+// 촘촘하고 빼죽하면서
+// 사이사이 라인이 그러진 피라미드 만들기
 Console.WriteLine("== 촘촘하게 ==");
 Console.WriteLine();
 foreach (var item in tower.lines)
@@ -19,11 +26,13 @@ foreach (var item in tower.lines)
     var leftSpace = cnt > 0 ? new String(' ', cnt) : string.Empty;
     // 라인 왼쪽 공간을 구현하기 
     var str = item.Insert(0, leftSpace);
-    // 별 그리기
+    // 피라미드 별 벽돌 그리기
     Console.WriteLine(str);
-    // 라인 그리기
+    // 피라미드 라인 계단 그리기
     Console.WriteLine(underLine.Insert(0, leftSpace));
 }
+
+// 한줄 띄기 (미용파트)
 Console.WriteLine();
 
 // 널찍하고 안정감 있게 피라미드 만들기
@@ -38,14 +47,18 @@ foreach (var item in tower.lines)
 
     // 별 라인 문자에서 사이 사이에 공간 문자 넣기 표현식
     var rs = System.Text.RegularExpressions.Regex.Replace(str, ".{1}", "$0 ");
-    // 그리기
+    // 피라미드 그리기
     Console.WriteLine(rs);
 }
-Console.WriteLine();
+
 Console.WriteLine();
 Console.WriteLine($"참조 => Last Of List -> {tower.lines.Last()}");
 Console.WriteLine($"참조 => First Of List => {tower.lines.First()}");
+Console.WriteLine();
 
+/// <summary>
+/// 피라미드 구현 클래스 라이브러리
+/// </summary>
 public class Tower
 {
     // 층수에 따라 바닥 타일 수 설정 속성
@@ -81,16 +94,15 @@ public class Tower
         // Call Stack (콜 스택 쌓기)
         MakeTower(number - 2, draw);
 
-        // 다 만들고 나서 스택으로 부터 하나썩 꺼내 
+        // 다 만들고 나서 콜스택으로 부터 하나썩 꺼내 
         // 각 층별로 별표 찍기 문자열 생성
-        var line = new string(draw, number);
-
         // 추가 가공을 위하여 목록에 저장해 놓기
-        lines.Add(line);
+        lines.Add(new string(draw, number));
     }
 }
+
 //== 재귀함수가 작동하는, 돌아가는 알고리즘 해석 ==//
-// Call Stack Orders 
+// Call Stack Orders , 7층을 기준으로 함.
 // 재귀함수가 콜스택에 쌓고(1층이 스택바닥) 빼내는 순서 //
 //================================================================================================//
 // 1. 층 13개   -> return 6 -> List index 6 : 즉, 리스트 목록에 마지막 인덱스에 바닥 별(*)이 들어가 있음 
