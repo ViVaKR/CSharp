e42bb0197a16afc140fcfda910ffc9865648ba8c
ViVaKR <admin@vivabj.com>
2023-04-02 15:48:34 +0900

    Add BootCamp

---
+++ Solutions/Recursion/Program.cs
@@ -0,0 +1,20 @@
+﻿
+var binary = new DecimalToBinary();
+
+
+
+public class DecimalToBinary
+{
+    public DecimalToBinary()
+    {
+        string binary = FindBinary(232, "");
+    }
+
+    public string FindBinary(int number, string result)
+    {
+        if(number == 0) return result;
+
+        result = number % 2 + result;
+        return FindBinary(number % 2, result);
+    }
+}
