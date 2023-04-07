3f2f969d45060aa0b3b75674f505883790dab84a
ViVaKR <admin@vivabj.com>
2023-04-08 08:10:15 +0900

    Add Examples

--- AnswerArchive/Solutions/Exampls/Program.cs
+++ AnswerArchive/Solutions/Exampls/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,13 @@ namespace Exampls
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var get = new List<string> { "A", "B", "C", "D", "A", "S", "A" }
+                        .Select((p, i) => new { id = i, ch = p })
+                        .Where(x => x.ch.Equals("A")).ToList();
+            
+            Console.WriteLine("=============== 추 출 ===============");
+            Console.WriteLine(string.Join(", ", get.Select(x=> x.id)));
         }
     }
+
 }
-
-
