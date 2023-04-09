91808e00c73ce4d2d95586b83bfeb768f79cc5a5
ViVaKR <admin@vivabj.com>
2023-04-09 18:52:55 +0900

    Update Demo-Redis

--- Solution.Demos/Demo-Redis/Extensions/DistributedCacheExtensions.cs
+++ Solution.Demos/Demo-Redis/Extensions/DistributedCacheExtensions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Distributed;
 
@@ -9,12 +10,26 @@ namespace Demo_Redis.Extensions
     public static class DistributedCacheExtensions
     {
         public static async Task SetRecordAsync<T>(this IDistributedCache cache,
-        string recordId, // Key : 캐쉬된 항목의 고유 식별자로 사용됨 즉, 이것이 반복될 수 없다는 의미
-        T data, // 저장될 데이터, 모델의 배열 즉, 객체의 배열
-        TimeSpan? absoluteExpireTime = null,
-        TimeSpan? unusedExpireTime = null)
+            string recordId, // Key : 캐쉬된 항목의 고유 식별자로 사용됨 즉, 이것이 반복될 수 없다는 의미
+            T data, // 저장될 데이터, 모델의 배열 즉, 객체의 배열
+            TimeSpan? absoluteExpireTime = null,
+            TimeSpan? unusedExpireTime = null)
         {
+            var options = new DistributedCacheEntryOptions();
+            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60);
+            options.SlidingExpiration = unusedExpireTime;
 
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(recordId, jsonData, options);  // pass key, value, options save data
+        }
+
+        public static async Task<T?> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+        {
+            var jsonData = await cache.GetStringAsync(recordId);
+
+            if (jsonData is null) return default(T);
+
+            return JsonSerializer.Deserialize<T>(jsonData);
         }
     }
 }
--- Solution.Demos/Demo-Redis/README.md
+++ Solution.Demos/Demo-Redis/README.md
@@ -26,4 +26,27 @@
     }
 ```
 
+## $scan 0, $ hgetall `<key>`
+
+```bash
+    127.0.0.1:6379> scan 0
+    1) "0"
+    2) 1) "viv_redis_WeatherForecast_20230409_0634"
+    127.0.0.1:6379> scan 0
+    1) "0"
+    2) 1) "viv_redis_WeatherForecast_20230409_0635"
+    2) "viv_redis_WeatherForecast_20230409_0634"
+    127.0.0.1:6379> scan 0
+    1) "0"
+    2) 1) "viv_redis_WeatherForecast_20230409_0635"
+    127.0.0.1:6379> hgetall viv_redis_WeatherForecast_20230409_0635
+    1) "absexp"
+    2) "638166297928792660"
+    3) "data"
+    4) "[{\"Date\":\"2023-04-10\",\"TemperatureC\":13,\"TemperatureF\":55,\"Summary\":\"Sweltering\"},{\"Date\":\"2023-04-11\",\"TemperatureC\":52,\"TemperatureF\":125,\"Summary\":\"Hot\"},{\"Date\":\"2023-04-12\",\"TemperatureC\":44,\"TemperatureF\":111,\"Summary\":\"Freezing\"},{\"Date\":\"2023-04-13\",\"TemperatureC\":-13,\"TemperatureF\":9,\"Summary\":\"Mild\"},{\"Date\":\"2023-04-14\",\"TemperatureC\":36,\"TemperatureF\":96,\"Summary\":\"Bracing\"}]"
+    5) "sldexp"
+    6) "-1"
+127.0.0.1:6379>
+```
+
 ## Ref : [IAmTimCorey(YouTube)](https://youtu.be/UrQWii_kfIE)
