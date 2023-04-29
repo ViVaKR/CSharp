850b2b83f60f7159ed980781dd78b038e6eb8e02
ViVaKR <admin@vivabj.com>
2023-04-29 22:59:30 +0900

    Update Watcher For [Jisikin]

--- Utils/VivProcess/MainForm.Designer.cs
+++ Utils/VivProcess/MainForm.Designer.cs
@@ -28,24 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Btn_Start = new System.Windows.Forms.Button();
+            this.Btn_StartAdmin = new System.Windows.Forms.Button();
             this.Lbl_Status = new System.Windows.Forms.Label();
             this.Btn_KillProcess = new System.Windows.Forms.Button();
             this.Grp_ControlBox = new System.Windows.Forms.GroupBox();
+            this.Btn_StartNormal = new System.Windows.Forms.Button();
             this.Grp_ControlBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Btn_Start
+            // Btn_StartAdmin
             // 
-            this.Btn_Start.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Btn_Start.Location = new System.Drawing.Point(284, 37);
-            this.Btn_Start.Margin = new System.Windows.Forms.Padding(4);
-            this.Btn_Start.Name = "Btn_Start";
-            this.Btn_Start.Size = new System.Drawing.Size(609, 66);
-            this.Btn_Start.TabIndex = 0;
-            this.Btn_Start.Text = "Start Process Watcher";
-            this.Btn_Start.UseVisualStyleBackColor = true;
-            this.Btn_Start.Click += new System.EventHandler(this.Btn_Start_Click);
+            this.Btn_StartAdmin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn_StartAdmin.Location = new System.Drawing.Point(284, 37);
+            this.Btn_StartAdmin.Margin = new System.Windows.Forms.Padding(4);
+            this.Btn_StartAdmin.Name = "Btn_StartAdmin";
+            this.Btn_StartAdmin.Size = new System.Drawing.Size(365, 43);
+            this.Btn_StartAdmin.TabIndex = 0;
+            this.Btn_StartAdmin.Text = "Start Watcher (Admin)";
+            this.Btn_StartAdmin.UseVisualStyleBackColor = true;
+            this.Btn_StartAdmin.Click += new System.EventHandler(this.Btn_Start_Admin_Click);
             // 
             // Lbl_Status
             // 
@@ -54,7 +55,7 @@
             this.Lbl_Status.Location = new System.Drawing.Point(0, 0);
             this.Lbl_Status.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_Status.Name = "Lbl_Status";
-            this.Lbl_Status.Size = new System.Drawing.Size(896, 344);
+            this.Lbl_Status.Size = new System.Drawing.Size(1001, 367);
             this.Lbl_Status.TabIndex = 1;
             this.Lbl_Status.Text = "...";
             this.Lbl_Status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -64,7 +65,7 @@
             this.Btn_KillProcess.Dock = System.Windows.Forms.DockStyle.Left;
             this.Btn_KillProcess.Location = new System.Drawing.Point(3, 37);
             this.Btn_KillProcess.Name = "Btn_KillProcess";
-            this.Btn_KillProcess.Size = new System.Drawing.Size(281, 66);
+            this.Btn_KillProcess.Size = new System.Drawing.Size(281, 43);
             this.Btn_KillProcess.TabIndex = 2;
             this.Btn_KillProcess.Text = "Kill Process";
             this.Btn_KillProcess.UseVisualStyleBackColor = true;
@@ -72,21 +73,34 @@
             // 
             // Grp_ControlBox
             // 
-            this.Grp_ControlBox.Controls.Add(this.Btn_Start);
+            this.Grp_ControlBox.Controls.Add(this.Btn_StartAdmin);
+            this.Grp_ControlBox.Controls.Add(this.Btn_StartNormal);
             this.Grp_ControlBox.Controls.Add(this.Btn_KillProcess);
             this.Grp_ControlBox.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.Grp_ControlBox.Location = new System.Drawing.Point(0, 344);
+            this.Grp_ControlBox.Location = new System.Drawing.Point(0, 367);
             this.Grp_ControlBox.Name = "Grp_ControlBox";
-            this.Grp_ControlBox.Size = new System.Drawing.Size(896, 106);
+            this.Grp_ControlBox.Size = new System.Drawing.Size(1001, 83);
             this.Grp_ControlBox.TabIndex = 3;
             this.Grp_ControlBox.TabStop = false;
             this.Grp_ControlBox.Text = "컨트롤";
+            // 
+            // Btn_StartNormal
+            // 
+            this.Btn_StartNormal.Dock = System.Windows.Forms.DockStyle.Right;
+            this.Btn_StartNormal.Location = new System.Drawing.Point(649, 37);
+            this.Btn_StartNormal.Margin = new System.Windows.Forms.Padding(4);
+            this.Btn_StartNormal.Name = "Btn_StartNormal";
+            this.Btn_StartNormal.Size = new System.Drawing.Size(349, 43);
+            this.Btn_StartNormal.TabIndex = 3;
+            this.Btn_StartNormal.Text = "Start Watcher (Normal)";
+            this.Btn_StartNormal.UseVisualStyleBackColor = true;
+            this.Btn_StartNormal.Click += new System.EventHandler(this.Btn_StartNormal_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 34F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(896, 450);
+            this.ClientSize = new System.Drawing.Size(1001, 450);
             this.Controls.Add(this.Lbl_Status);
             this.Controls.Add(this.Grp_ControlBox);
             this.Font = new System.Drawing.Font("KoPubWorldDotum Medium", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
@@ -100,10 +114,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button Btn_Start;
+        private System.Windows.Forms.Button Btn_StartAdmin;
         private System.Windows.Forms.Label Lbl_Status;
         private System.Windows.Forms.Button Btn_KillProcess;
         private System.Windows.Forms.GroupBox Grp_ControlBox;
+        private System.Windows.Forms.Button Btn_StartNormal;
     }
 }
 
--- Utils/VivProcess/MainForm.cs
+++ Utils/VivProcess/MainForm.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Windows.Forms;
+using System.Management;
+using System.Windows.Forms; // 타이머
+
 
 namespace VivProcess
 {
     public partial class MainForm : Form
     {
         // 감시할 프로세스 이름 : 확장자 포함
-        private readonly string proc = "notepad.exe";
+        private const string proc = "notepad.exe";
+        private string title = string.Empty;
+        private const string started = "**** Started *****";
+        private const string stopped = "**** Stopped *****";
 
-        private readonly Timer timer;
+        private readonly System.Windows.Forms.Timer timer;
+
+        //* 타이머 오류 처리 *//
+        // System.Threading.Timer 사이에 모호한 참조 -> 라인 제거
+        //
+        //-> System.Windows.Forms.Timer timer;
+
         private readonly Stopwatch watch;
         private readonly Proc ps;
         private bool isWatch;
@@ -22,17 +33,20 @@ namespace VivProcess
             Width = 1024;
             Height = 768;
             StartPosition = FormStartPosition.CenterScreen;
+
+            // 버튼폭 조절 (폼크기 변경시)
+            ResizeEnd += (s, e) => SetButtonsWidth();
+
             Btn_KillProcess.Enabled = false;
             isWatch = false;
-            Btn_Start.Text = isWatch ? "Stop Watcher" : "Start Watcher";
+            Btn_StartAdmin.Text = isWatch ? "Stop Watcher" : "Start Watcher";
             // 프로세서 
             ps = new Proc();
 
             // 타이머
-            timer = new Timer
-            {
-                Interval = 1000
-            };
+            timer = new Timer { Interval = 1000 }; // 매초
+            // timer = new Timer { Interval = 1000 * 60 * 60 }; // 60 분
+
             timer.Tick += Timer_Tick;
             timer.Start();
 
@@ -40,18 +54,30 @@ namespace VivProcess
             watch = new Stopwatch();
         }
 
+        private void SetButtonsWidth()
+        {
+            Btn_KillProcess.Width = Width / 5;
+            Btn_StartAdmin.Width = Width * 2 / 5;
+            Btn_StartNormal.Width = Width * 2 / 5;
+        }
+
         private void Timer_Tick(object sender, EventArgs e)
         {
-            string message = watch.Elapsed.Seconds == 0 ? $"{proc} Not Runnig..." : $"{proc} Started... {watch.Elapsed.Seconds} 초 경과";
-            Message(message);
+            string message = watch.Elapsed.Seconds == 0
+                ? $"{title}\n{proc} Not Runnig..."
+                : $"{title}\n{proc} Started... {watch.Elapsed.Seconds} 초 경과";
+
+            Lbl_Status.Text = $"{message}";
+
             Application.DoEvents();
         }
 
-        private void Btn_Start_Click(object sender, EventArgs e)
+        #region Admin Privilege Watcher
+        private void Btn_Start_Admin_Click(object sender, EventArgs e)
         {
             // 프로세스 시작 및 중지 이벤트
-            ps.startWatcher.EventArrived += StartWatch_EventArrived;
-            ps.stopWatcher.EventArrived += StopWatch_EventArrived;
+            ps.watcher_start_admin.EventArrived += Watcher_Started_Admin_EventArrived;
+            ps.watcher_stop_admin.EventArrived += Watcher_Stopped_Admin_EventArrived;
 
             // 감시시작
             ps.StartWatch();
@@ -60,19 +86,12 @@ namespace VivProcess
 
             // 버튼 토글시 감시중지
             if (!isWatch) ps.StopWatch();
+            else ps.StartWatch();
 
-            Btn_Start.Text = isWatch ? "Stop Watcher" : "Start Watcher";
-
+            Btn_StartAdmin.Text = isWatch ? "Stop Watcher (Admin)" : "Start Watcher (Admin)";
         }
 
-        private void Message(string message) => Lbl_Status.Text = $"{message}";
-
-        /// <summary>
-        /// 모든 프로세스 중지 감지 이벤트
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void StopWatch_EventArrived(object sender, System.Management.EventArrivedEventArgs e)
+        private void Watcher_Stopped_Admin_EventArrived(object sender, EventArrivedEventArgs e)
         {
             // 중지된 프로세스가 감시 목록에 있는 것인지 이름으로 확인
             string procName = e.NewEvent.Properties["ProcessName"]?.Value?.ToString();
@@ -84,12 +103,7 @@ namespace VivProcess
             SetKillButton(false);
         }
 
-        /// <summary>
-        /// 모든 프로세스 시작 감지 이벤트 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void StartWatch_EventArrived(object sender, System.Management.EventArrivedEventArgs e)
+        private void Watcher_Started_Admin_EventArrived(object sender, EventArrivedEventArgs e)
         {
             // 시작된 프로세스가 감시 목록에 있는 것인지 이름으로 확인
             string procName = e.NewEvent.Properties["ProcessName"]?.Value?.ToString();
@@ -98,24 +112,73 @@ namespace VivProcess
             watch.Start();
 
             SetKillButton(true);
-
         }
 
+        #endregion
+
+
+        #region Normal Privilege Watcher
+
+        private void Btn_StartNormal_Click(object sender, EventArgs e)
+        {
+            ps.watcher_start_normal.EventArrived += Watcher_Started_Normal_EventArrived;
+            ps.watcher_stop_normal.EventArrived += Watcher_Stopped_Normal_EventArrived;
+
+            isWatch = !isWatch;
+
+            Btn_StartNormal.Text = (isWatch = !isWatch) ? "Stop Watcher (Normal)" : "Start Watcher (Normal)";
+
+            if (!isWatch) ps.StopNormal();
+            else ps.StartNormal();
+            ps.StartNormal();
+        }
+
+        private void Watcher_Stopped_Normal_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject targetInstance = (ManagementBaseObject)e.NewEvent.Properties["TargetInstance"].Value;
+            string processName = targetInstance.Properties["Caption"].Value?.ToString();
+            if (!processName.Equals(proc)) return;
+
+            uint processId = (uint)targetInstance.Properties["ProcessId"].Value;
+            uint parentProcessId = (uint)targetInstance.Properties["ParentProcessId"].Value;
+            string path = targetInstance.Properties["ExecutablePath"].Value?.ToString();
+
+            title = $"{stopped}\n{processName}\n- Process ID: {processId}\n- Parent ID: {parentProcessId}\n- File Path: {path}\n";
+            // 초기화
+            watch.Stop();
+            watch.Reset();
+            SetKillButton(false);
+        }
+
+        private void Watcher_Started_Normal_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject targetInstance = (ManagementBaseObject)e.NewEvent.Properties["TargetInstance"].Value;
+            string processName = targetInstance.Properties["Caption"].Value?.ToString();
+            if (!processName.Equals(proc)) return;
+            watch.Start();
+
+
+            uint processId = (uint)targetInstance.Properties["ProcessId"].Value;
+            uint parentProcessId = (uint)targetInstance.Properties["ParentProcessId"].Value;
+            string path = targetInstance.Properties["ExecutablePath"].Value?.ToString();
+
+            // 풍부한 정보 제공용
+            title = $"{started}\n{processName}\n- Process ID: {processId}\n- Parent ID: {parentProcessId}\n- File Path: {path}\n";
+            SetKillButton(true);
+        }
+
+        #endregion
+
+        #region Kill Process
         private void SetKillButton(bool tf)
         {
             BeginInvoke((MethodInvoker)delegate
             {
                 Btn_KillProcess.Enabled = tf;
-
                 Btn_KillProcess.Text = tf ? $"Kill -> {proc}" : "Watching...";
             });
         }
 
-        /// <summary>
-        /// 프로세스 이름으로 죽이기
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void Btn_KillProcess_Click(object sender, EventArgs e)
         {
             foreach (Process process in Process.GetProcessesByName(Path.GetFileNameWithoutExtension(proc)))
@@ -123,5 +186,6 @@ namespace VivProcess
                 process.Kill();
             }
         }
+        #endregion
     }
 }
