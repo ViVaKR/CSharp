9c6049eb7ed6510f2d4969e28571e8974befc80a
ViVaKR <admin@vivabj.com>
2023-05-02 17:45:12 +0900

    Add Jisikin Ping Test

--- Solution.Demos/Demo-JiSikin/Program.cs
+++ Solution.Demos/Demo-JiSikin/Program.cs
@@ -2,6 +2,7 @@
 Console.Write("메뉴선택 >> ");
 Console.WriteLine("1. Money");
 Console.WriteLine("2. UDP");
+Console.WriteLine("3. Ping");
 
 int choice;
 bool check = int.TryParse(Console.ReadLine(), out choice);
@@ -30,6 +31,28 @@ switch (choice)
             // UDP 접속하기
             nets.UdpClient();
             nets.StopAll();
+        }
+        break;
+    case 3:
+        {
+            await Task.Run(() =>
+            {
+                string subnet = "172.30.1";
+                Console.WriteLine("***** 핑테스트를 시작합니다. *****");
+                for (int i = 250; i < 257; i++) // 없는 아이피 포함
+                {
+                    var ip = $"{subnet}.{i}";
+                    ip.Ping();
+                }
+
+                var google = $"8.8.8.8";
+                google.Ping();
+
+                var dm = "164.124.101.1";
+                dm.Ping();
+
+                
+            });
         }
         break;
 
