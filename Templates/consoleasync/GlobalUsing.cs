6b4478e22faf5fecbd93ac0b45a6b8b314a4de91
ViVaKR <admin@vivabj.com>
2023-05-27 15:25:14 +0900

    Add Templates

---
+++ Templates/consoleasync/GlobalUsing.cs
@@ -0,0 +1,8 @@
+global using System;
+global using System.IO;
+global using System.Text;
+global using System.Diagnostics;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Runtime.InteropServices;
+global using System.Reflection;
---
+++ Templates/consoleasync/README.md
@@ -0,0 +1,5 @@
+# Project Readme
+
+```bash
+    $ dotnet new install ./
+```
