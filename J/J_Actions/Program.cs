9e36ecbf947c6b6bd0a4bf6258aaf455b275a37f
ViVaKR <admin@vivabj.com>
2023-06-15 08:15:53 +0900

    Update J_Actions

--- J/J_Actions/Program.cs
+++ J/J_Actions/Program.cs
@@ -1,5 +1,5 @@
 ﻿using J_Actions;
-
+// ㅇ아hello world fine thanks and you 방가방가 합니다.
 Console.WriteLine("*** Action Demo ***");
 Console.WriteLine();
 
@@ -11,3 +11,4 @@ Action<int, int> action2 = MainApp.Resize;
 // Combine Action 1 += Action 2
 action1 += action2;
 action1(200, 300);
+
