5e1b8840f8650c10b2dc32b8a076a2ceb6586ef2
ViVaKR <admin@vivabj.com>
2023-08-07 04:18:36 +0900

    delete VivProcess.csproj

---
+++ Solution.Tutorial/Tutor.Abstract/Square.cs
@@ -0,0 +1,15 @@
+﻿namespace Tutor.Abstract;
+
+public abstract class Shape
+{
+    public abstract int GetArea();
+}
+
+public class Square : Shape
+{
+    private readonly int _side;
+
+    public Square(int side) => _side = side;
+
+    public override int GetArea()=> _side * _side;
+}
