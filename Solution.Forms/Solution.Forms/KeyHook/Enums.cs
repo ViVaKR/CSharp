4a2b1e664b466bc1a48b15e7b1fd9309e09c8d2a
Kim Bum Jun <admin@vivabj.com>
2023-09-20 16:20:37 +0900

    Add Solution.Forms

---
+++ Solution.Forms/Solution.Forms/KeyHook/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace KeyHook
+{
+    /// <summary>
+    /// The enumeration of possible modifiers.
+    /// </summary>
+    [Flags]
+    public enum KeyModifiers : uint
+    {
+        Alt = 1,
+        Control = 2,
+        Shift = 4,
+        Win = 8
+    }
+}
