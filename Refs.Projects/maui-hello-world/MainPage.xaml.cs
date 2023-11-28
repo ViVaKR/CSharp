bb12d3d7a94a4e49fcb58eced43291aa06dc15de
Kim Bum Jun <admin@vivabj.com>
2023-11-29 00:51:06 +0900

    maui-hello-world [new]

---
+++ Refs.Projects/maui-hello-world/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace maui_hello_world;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		var rs = Color.FromRgb(255, 255, 0);
+	}
+
+}
+
