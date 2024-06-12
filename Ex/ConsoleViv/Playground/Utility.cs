ea2329bfaef6e65abbd6a2323a5e6d97c5d1eff2
Kim Bum Jun <admin@vivabj.com>
2024-06-12 11:22:11 +0900

    Create new Playground/Utility

---
+++ Ex/ConsoleViv/Playground/Utility.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace ConsoleViv;
+
+public static class Utility
+{
+    static readonly HttpClient client = new();
+
+    public static async Task GetExternalIp()
+    {
+        try
+        {
+            using HttpResponseMessage response = await client.GetAsync("https://api.ipify.org");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+        }
+        catch (System.Exception ex)
+        {
+            Console.WriteLine($"Exception caught.\n{ex.Message}");
+        }
+    }
+
+}
--- Ex/ConsoleViv/Program.cs
+++ Ex/ConsoleViv/Program.cs
@@ -1,20 +1,30 @@
 ﻿
 using ConsoleViv;
 
-// Caller
-await SimpleTask.RunAsyncA().ContinueWith(x =>
+// int k = 10;
+// int t = 5;
+// if ((k -= t) == 5)
+// {
+//     ;
+// }
+
+// // Caller
+// await SimpleTask.RunAsyncA().ContinueWith(x =>
+// {
+//     SimpleTask.RunB();
+
+// }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+// Console.Clear();
+// Console.WriteLine("[ ref vs out 차이점 ]");
+
+// int a = 10; // ref 는 초기화 필요
+// int b = 5;
+// int sum;    // out 은 초기화 불 필요
+// SimpleTask.Add(ref a, b, out sum); // Call by reference
+// Console.WriteLine($"{sum} = {a} + {b}"); // 15
+
+await Task.Run(async () =>
 {
-    SimpleTask.RunB();
-
-}, TaskContinuationOptions.OnlyOnRanToCompletion);
-
-Console.Clear();
-Console.WriteLine("[ ref vs out 차이점 ]");
-
-int a = 10; // ref 는 초기화 필요
-int b = 5;
-int sum;    // out 은 초기화 불 필요
-SimpleTask.Add(ref a, b, out sum); // Call by reference
-Console.WriteLine($"{sum} = {a} + {b}"); // 15
-
-
+    await Utility.GetExternalIp();
+});
