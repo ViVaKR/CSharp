c3f6ed15152b8d081bbcba94e00d140caed0246f
Kim Bum Jun <admin@vivabj.com>
2024-06-13 04:42:41 +0900

    Modified AdoNetEx

--- Ex/AdoNetEx/Program.cs
+++ Ex/AdoNetEx/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdoNetEx;
+
+// --> 선행 작업 --> [ 데이터 모델링, 추상화 : 데이터베이스 테이블을 클래스로 매핑 ]
+// 1. 데이터 전체를 가져오기
+// 2. 데이터가 없을 경우 처리
+// 3. 데이터 추출 - (샘플) Id 3번 부터 3개만 추출 후 출력
+// 4. 레이블 인덱스 가상 넘버링
+// 5. 선택된 데이터 출력
+
+// 데이터 가져오기
+var data = await Utility.GetColorTableAsync();
+
+// 데이터가 없을 경우 처리
+if (data is null) return;
+
+// 데이터 추출 - Id 3번 부터 3개만 추출 후 출력
+var selection = data.Where(x => x.Id >= 3).Take(3).ToList();
+
+int index = 1; // 레이블 인덱스 가상 넘버링
+foreach (var item in selection)
+{
+    Console.WriteLine($"(가상 라벨 컨트롤) label{index++}.text = {item.HexCode}");
+    Console.WriteLine($"(가상 라벨 컨트롤) label{index++}.text = {item.DecimalCode}");
+}
---
+++ Ex/AdoNetEx/Utility.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+namespace AdoNetEx;
+
+public class ColorTable
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string HexCode { get; set; }
+    public required string DecimalCode { get; set; }
+}
+
+public class Utility
+{
+    public static async Task<List<ColorTable>?> GetColorTableAsync()
+    {
+        using SqlConnection connection = new("connectionString");
+        await connection.OpenAsync(); // 비동기로 연결 열기
+        using var command = connection.CreateCommand(); // 명령 객체 생성
+        command.CommandText = "SELECT * FROM ColorTables;"; // SQL 명령 설정
+        using var reader = await command.ExecuteReaderAsync(); // 데이터 읽기
+        List<ColorTable> tables = []; // 데이터 테이블 목록 생성
+        while (await reader.ReadAsync())
+        {
+            ColorTable table = new()
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                Name = reader["Name"]?.ToString() ?? string.Empty,
+                HexCode = reader["HexCode"]?.ToString() ?? string.Empty,
+                DecimalCode = reader["DecimalCode"]?.ToString() ?? string.Empty
+            };
+            tables.Add(table);
+        }
+        return tables;
+    }
+}
